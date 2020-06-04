046bd48927f49f567bbded6de076362382b946ff
Samson Bekele <bekelesamson4@gmail.com>
2020-06-04 02:22:08 -0600

    Actual and final development

--- stats.c
+++ stats.c
@@ -1,7 +1,7 @@
 /******************************************************************************
  * Copyright (C) 2020 by Samson Bekele
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files this copyright. Users are 
  * permitted to modify this and use it to learn about the field of embedded
  * software. Samson Bekele is  not liable for any
  * misuse of this material. 
@@ -40,13 +40,27 @@ void main() {
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+  /* Statistics and  printing Functions Go Here */
+/*unsigned char min = find_minimum(test,SIZE);
+unsigned char max = find_maximum(test,SIZE);
+unsigned char  mean = find_mean(test,SIZE);
+unsigned char  median = find_median(test,SIZE);
 
+printf("mean %d ",mean);*/
+
+     print_statistics(test,SIZE);
+// sort_array(test,SIZE);
+// print_array(test,SIZE);
+/* for(unsigned int i=0;i<SIZE;i++)
+ {
+	 printf("%d",test[i]);
+
+ }
+*/ 
 }
-
 /* Add other Implementation File Code Here */
 
-
+ 
 /******************************************************************************
  *Function: print_statistics
  *Description:
@@ -54,66 +68,96 @@ void main() {
  *
  *Parameters:
  *          
- *       unsigned char  * ptr: Pointer to a data set
- *        unsigned char count: Number of items in data set
+ *       unsigned char  * ptr: A unsigned char pointer to an n-element data array
+ *        unsigned int count: Number of items in data set
  *         
  *
  *Return:
  *      print the statistics of an array 
 *******************************************************************/
-unsigned char print_statistics (unsigned char * ptr, unsigned char  count){
+void  print_statistics (unsigned char * ptr, unsigned int count){
+	unsigned char max;
+	unsigned char min;
+	unsigned char median;
+	         min = find_minimum(ptr,count);
+		  max = find_maximum(ptr,count);
+        unsigned char mean = find_mean(ptr,count);
+		 median = find_median(ptr,count);
+		   printf("The maximum value %d\n",max);
+		   printf("The minimum  value %d\n",min);
+		   printf("The mean  value %d\n",mean);
+		   printf("The median value %d\n",median);
 }
 
 /**************************************************************************
  * function: print_array
  *    Description:given an array of data and a length,prints the array to the screen
  *    Parameters:
- *             unsigned char  * ptr: Pointer to a data set
- *             unsigned char  count: Number of items in data set
+ *             unsigned char  * ptr: A unsigned char pointer to an n-element data array
+ *             unsigned int count: Number of items in data set
  *     Return: 
  *          prints the array to the screen.
  ************************************************************************************/
- unsigned char print_array (unsigned char * ptr, unsigned char  count){
-
+ void print_array (unsigned char * ptr, unsigned int count){
+            for(unsigned int i = 0;i<count;i++)
+	    {
+		    printf(" %d\t ", ptr[i]);
+				   
+            }
  }
+ 
 /**************************************************************
  *function: find_median
  *Description:given an array of data and a length,returns the median value 
  *Parameters:
- *          unsigned char  * ptr: Pointer to a data set
- *          unsigned char  count: Number of items in data set
+ *          unsigned char  * ptr: A unsigned char pointer to an n-element data array
+ *          unsigned int count: Number of items in data set
  *Return:
  *     returns the median
  *************************************************************************************/
- unsigned char find_median(unsigned char * ptr, unsigned char  count){
-
-	
+ unsigned char find_median(unsigned char * ptr, unsigned int count){
+                             sort_array(ptr,count);
+			     unsigned char  median=0;
+	  if(count%2 == 0) median = (ptr [(count/2)-1] + ptr [count/2])/2;
+		           else median=ptr [count/2];	  
+	            return median;
  }
+ 
 /**************************************************************
  *function: find_mean
  *Description:given an array of data and a length,returns the mean  
  *Parameters:
- *          unsigned char  * ptr: Pointer to a data set
- *          unsigned char  count: Number of items in data set
+ *          unsigned char  * ptr: A unsigned char pointer to an n-element data array
+ *          unsigned int count: Number of items in data set
  *Return: 
  *      returns the mean
  * *************************************************************************************/
-  unsigned char find_mean(unsigned char * ptr, unsigned char  count){
-
+  unsigned char find_mean(unsigned char * ptr, unsigned int count){
+                      unsigned char mean=0;
+		      for(unsigned char i=0;i < count;i++) mean += ( ptr[i]/count);
+		   return   mean;
 	      
- 
-  }
+           }
+
 /**************************************************************
  *function: find_maximum
  *Description:given an array of data and a length,returns the mean  
  *Parameters
- *          unsigned char  * ptr: Pointer to a data set
- *          unsigned char  count: Number of items in data set
+ *          unsigned char  * ptr: A unsigned char pointer to an n-element data array
+ *                 unsigned int count: Number of items in data set
  *Return: 
  *         returns the maximution: 
   *******************************************************************************/
-unsigned char find_maximum(unsigned char * ptr, unsigned char  count){
+unsigned char find_maximum(unsigned char * ptr,unsigned int count){
 
+        unsigned char max;
+	max=ptr[0];
+               for(unsigned char i=0;i < count;i++)
+		  {
+                     if(max < ptr[i]) max = ptr[i];
+
+                   }
+	       return max;
 
 	      
     
@@ -122,27 +166,47 @@ unsigned char find_maximum(unsigned char * ptr, unsigned char  count){
  *function: find_minimum
  *description:given an array of data and a length,returns the minimum   
  *Parameters
- *          unsigned char  * ptr: Pointer to a data set
- *          unsigned char  count: Number of items in data set
+ *          unsigned char  * ptr: A unsigned char pointer to an n-element data array
+ *          unsigned char  char  count: Number of items in data set
  *Return: 
  *         returns the minimum
 ****************************************************************/
- unsigned char find_minimum(unsigned char * ptr, unsigned char  count){
+ unsigned char find_minimum(unsigned char * ptr, unsigned int count){
+             unsigned char min = ptr[0];
+	    for(unsigned char i=0;i < count;i++)
+	       {
+                  if( min > ptr[i]) min = ptr[i];
 
+               }
+
+	    return min;
+ 
  }
 /**************************************************************
  *function:sort_array
  *description:given an array of data and a length,sorts the array from largest to smallest  
  *    
  *Parameters
- *               unsigned char  * ptr: Pointer to a data set
- *               unsigned char  count: Number of items in data set
+ *               unsigned char  * ptr: A unsigned char pointer to an n-element data array
+ *               unsigned int count: Number of items in data set
  *Return: 
  *      sorts the array from the largest to smallest
  *****************************************************************/
- unsigned char sort_array(unsigned char * ptr, unsigned char  count){
-
+ void sort_array(unsigned char * ptr, unsigned int count){
+                        
+	               unsigned int i;
+		       int j;
+		       unsigned char temp;
+		   for(i=0;i<count;i++)
+		      {
+		         temp=ptr[i];
+		         j=i-1;	
+			while(j>=0 && ptr[j] < temp)
+		            {
+		              ptr[j+1]=ptr[j];
+	                      j--;
+	                     }
+                        ptr[j+1]=temp;
+                      }			
 	  }
 
-
-	
--- stats.h
+++ stats.h
@@ -1,6 +1,5 @@
-/******************************************************************************
- * Copyright (C) 2020 by Samson Bekele
- *
+ /***************************************************
+ *Copyright (C)17-May-17-May-2020
  * Redistribution, modification or use of this software in source or binary
  * forms is permitted as long as the files maintain this copyright. Users are 
  * permitted to modify this and use it to learn about the field of embedded
@@ -27,61 +26,63 @@
  *           
  *@return print the statistics of an array 
  ***************************************************************************/
-unsigned char print_statistics (unsigned char * ptr, unsigned char  count);
+void  print_statistics (unsigned char * ptr, unsigned int  count);
 
 /**************************************************************************
  *@brief given an array of data and a length,prints the array to the screen
 
  *@param unsigned char  * ptr: Pointer to a data set
- *@param unsigned char  count: Number of items in data set
+ *@param unsigned int  count: Number of items in data set
 
  *@return prints the array to the screen.
  *         ************************************************************************************/
- unsigned char print_array (unsigned char * ptr, unsigned char  count);
+ void print_array (unsigned char * ptr, unsigned int  count);
 
   /**********************************************************************
  *@brief given an array of data and a length,returns the median value 
   *
   *
    *@param unsigned char  * ptr: Pointer to a data set
-   *@param unsigned char  count: Number of items in data set
+   *@param unsigned int  count: Number of items in data set
    *
    *@return returns the median
    ***********************************************************************************************/
-    unsigned char find_median(unsigned char * ptr, unsigned char  count);
+    unsigned char find_median(unsigned char * ptr, unsigned int  count);
 /**************************************************************
  *@brief given an array of data and a length,returns the mean  
  *
  *@param unsigned char  * ptr: Pointer to a data set
- *@param  unsigned char  count: Number of items in data set
+ *@param  unsigned int  count: Number of items in data set
  *
  *@return returns the mean
  *************************************************************************************/
-      unsigned char find_mean(unsigned char * ptr, unsigned char  count);
+      unsigned char find_mean(unsigned char * ptr, unsigned int  count);
       /**************************************************************
        *
        *@brief given an array of data and a length,returns the mean  
        *
        *@param unsigned char  * ptr: Pointer to a data set
-       *@param unsigned char  count: Number of items in data set
+       *@param unsigned int  count: Number of items in data set
        *        
        *@return   returns the maximum
        ********************************************************************************/
-      unsigned char find_maximum(unsigned char * ptr, unsigned char  count);
+      unsigned char find_maximum(unsigned char * ptr, unsigned int  count);
 /**************************************************************
  *@brief  given an array of data and a length,returns the minimum   
  *
  *@param  unsigned char  * ptr: Pointer to a data set
- *@param  unsigned char  count: Number of items in data set
+ *@param  unsigned int  count: Number of items in data set
  *        
  *@return  returns the minimum
  *****************************************************************/
- unsigned char find_minimum(unsigned char * ptr, unsigned char  count);
+ unsigned char find_minimum(unsigned char * ptr, unsigned int  count);
 /**************************************************************
  *@brief given an array of data and a length ,sorts the array from te largest to smallest 
  *@param unsigned char  * ptr: Pointer to a data set
- *@param  unsigned char  count: Number of items in data set
+ *@param  unsigned int  count: Number of items in data set
  *         
  *@return  sorts the array from the largest to smallest
  ******************************************************************/
- unsigned char sort_array(unsigned char * ptr, unsigned char  count); 
+ void sort_array(unsigned char * ptr, unsigned int  count); 
+ 
+#endif /* __STATS_H__ */
